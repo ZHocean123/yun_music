65099d0167fdffb7f5b2efb0270a60cabbb37903
ZHocean123 <yushanghaiyang@gmail.com>
2017-11-13 13:25:23 +0800

    Initial Commit

---
+++ yun_musicTests/yun_musicTests.swift
@@ -0,0 +1,36 @@
+//
+//  yun_musicTests.swift
+//  yun_musicTests
+//
+//  Created by yang on 13/11/2017.
+//  Copyright © 2017 ocean. All rights reserved.
+//
+
+import XCTest
+@testable import yun_music
+
+class yun_musicTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
